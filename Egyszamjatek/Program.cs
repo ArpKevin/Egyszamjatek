24872997a6b178038501693d1f674f7ca2f1a2cd
C20ÁrpásK <C20rpsK@203-02>
2024-05-28 11:27:30 +0200

    Add project files.

---
+++ Egyszamjatek/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Egyszamjatek
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzok = new List<Versenyzo>();
+
+            foreach (var item in File.ReadAllLines(@"..\..\..\src\egyszamjatek1.txt"))
+            {
+                versenyzok.Add(new(item));
+            };
+
+            Console.WriteLine($"3. feladat: Játékosok száma: {versenyzok.Count} fő");
+
+
+            Console.Write("4. feladat: Kérem a forduló sorszámát: ");
+            int sorszam = int.Parse(Console.ReadLine()) - 1;
+
+            double atlag = versenyzok.Average(v => v.Tippek[sorszam]);
+
+            Console.WriteLine($"5. feladat: A megadott forduló tippjeinek átlaga: {Math.Round(atlag, 2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Egyszamjatek/Versenyzo.cs
@@ -0,0 +1,27 @@
+﻿namespace Egyszamjatek
+{
+    internal partial class Program
+    {
+        public class Versenyzo
+        {
+            public string Nev { get; set; }
+            public List<int> Tippek { get; set; }
+
+            public Versenyzo(string sor)
+            {
+                var x = sor.Split(" ");
+                Nev = x[0];
+                Tippek = new List<int>();
+                for (int i = 1; i < x.Length; i++)
+                {
+                    Tippek.Add(int.Parse(x[i]));
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{Nev}, {string.Join(";", Tippek)}";
+            }
+        }
+    }
+}
